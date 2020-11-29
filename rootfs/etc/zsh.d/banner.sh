050da6c03c8e769407d657c68622c45e296e13ec
OpsBots <47907782+opsbotdev@users.noreply.github.com>
2020-11-29 10:14:52 -0600

    add text banner

---
+++ rootfs/etc/zsh.d/banner.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+COLOR_RESET="[0m"
+BANNER_COMMAND="${BANNER_COMMAND:-figurine}"
+BANNER_COLOR="${BANNER_COLOR:-[36m}"
+BANNER_INDENT="${BANNER_INDENT:-    }"
+BANNER_FONT="${BANNER_FONT:-Nancyj.flf}"
+
+if [ "${SHLVL}" = "1" ]; then
+	# Display a banner message for interactive shells (if we're not in aws-vault or aws-okta)
+	(source /etc/os-release && printf "# Geodeisc based on %s\n\n" "$PRETTY_NAME")
+	if [ -n "${BANNER}" ]; then
+		if [ "$BANNER_COMMAND" = "figlet" ]; then
+			echo "${BANNER_COLOR}"
+			${BANNER_COMMAND} -w 200 "${BANNER}" | sed "s/^/${BANNER_INDENT}/"
+			echo "${COLOR_RESET}"
+		elif [ "$BANNER_COMMAND" = "figurine" ]; then
+			${BANNER_COMMAND} -f "${BANNER_FONT}" "${BANNER}" | sed "s/^/${BANNER_INDENT}/"
+		fi
+	fi
+fi
